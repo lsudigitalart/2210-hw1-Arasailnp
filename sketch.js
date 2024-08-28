326c026552f09f0d435e848580f443253003e7d0
Sara Plain <splain3@lsu.edu>
2024-08-28 14:38:14 -0500

    face with no mouth

---
+++ sketch.js
@@ -0,0 +1,22 @@
+function setup() {
+createCanvas(windowWidth, windowHeight);
+
+background(2, 0, 130);
+fill (200, 200, 255);
+noStroke()
+ellipse(windowWidth/2, windowHeight/2, 230, 230);
+fill(0)
+ellipse(225,200, 20 ,20)
+ellipse(325 ,200, 20 ,20)
+noFill()
+stroke (5)
+arc(275, 250, 80 ,-30, 30, 500)
+fill(200, 175, 255)
+arc (275, 250, 80 ,-30, 30, 500)
+
+
+
+}
+function draw() {
+
+}
