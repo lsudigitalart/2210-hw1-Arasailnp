4a27a73be170a7e2255fa9f8ed4478117177a283
Sara Plain <splain3@lsu.edu>
2024-09-04 12:49:30 -0500

    face with no mouth yet

--- sketch.js
+++ sketch.js
@@ -2,17 +2,26 @@ function setup() {
 createCanvas(windowWidth, windowHeight);
 
 background(2, 0, 130);
+
 fill (200, 200, 255);
 noStroke()
 ellipse(windowWidth/2, windowHeight/2, 230, 230);
 fill(0)
 ellipse(225,200, 20 ,20)
 ellipse(325 ,200, 20 ,20)
+fill(300)
+ellipse(220,200, 20/2,20/2)
+ellipse(320 ,200, 20/2 ,20/2)
+fill(0)
+ellipse(275, 300,40,20)
+fill(200, 100, 150)
+ellipse(285, 300,20,15)
 noFill()
 stroke (5)
 arc(275, 250, 80 ,-30, 30, 500)
 fill(200, 175, 255)
 arc (275, 250, 80 ,-30, 30, 500)
+
 
 
 
